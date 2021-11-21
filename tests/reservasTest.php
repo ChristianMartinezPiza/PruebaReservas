986857ac5fcecb80b805003f9226cf871fade34d
Christian Martínez Pizá <cmartinezp@esliceu.net>
2021-11-21 04:13:40 +0100

    Creado testing.

---
+++ tests/reservasTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Tests;
+
+use App\Entity\Reservas;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use function PHPUnit\Framework\greaterThan;
+
+class reservasTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $reservas = new Reservas();
+        $reservasArray = $reservas->actualizarReservas();
+        $reservasJson = json_encode($reservasArray);
+        file_put_contents('reservas.json', $reservasJson);
+        $this->assertGreaterThan(2, $reservasArray);
+        $this->assertFileExists('reservas.json');
+    }
+}
